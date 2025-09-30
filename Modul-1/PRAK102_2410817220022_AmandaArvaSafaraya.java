083593f0e4e00382f50315d65f64c8c709e5071e
Amanda-06 <mnd.06.as@gmail.com>
2025-09-30 21:02:08 +0700

    Add files via upload

---
+++ Modul-1/PRAK102_2410817220022_AmandaArvaSafaraya.java
@@ -0,0 +1,28 @@
+package PRAKTIKUM_1;
+import java.util.Scanner;
+public class PRAK102_2410817220022_AmandaArvaSafaraya {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("");
+        int angkaAwal = input.nextInt();
+        input.close();
+
+        int i = 0;
+        while (i <= 10) {
+            int bilangan = angkaAwal + i;
+            if (bilangan % 5 == 0) {
+                System.out.print((bilangan / 5) - 1);
+            } else {
+                System.out.print(bilangan);
+            }
+
+            if (i < 10) {
+                System.out.print(", ");
+            }
+            i++;
+        }
+        System.out.println(); 
+    }
+}
---
+++ Modul-1/PRAK103_2410817220022_AmandaArvaSafaraya.java
@@ -0,0 +1,30 @@
+package PRAKTIKUM_1;
+import java.util.Scanner;
+public class PRAK103_2410817220022_AmandaArvaSafaraya {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int bilanganAwal = input.nextInt();
+        input.close();
+
+        int i = 0; 
+        int angka = bilanganAwal; 
+
+        do {
+            if (angka % 2 != 0) {
+                System.out.print(angka);
+                i++; 
+
+                if (i < n) {
+                    System.out.print(", ");
+                }
+            }
+            angka++;
+        } while (i < n);
+        
+        System.out.println();
+    }
+
+	}
